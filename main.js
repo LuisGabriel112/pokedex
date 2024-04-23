4180b4eb5d96f1b7f9d7388279b7843e178a0db0
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2024-04-23 07:34:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// Buscar Pokémon
+const buscarPokemon = (contenedorNum) => {
+  let inputId = `pokemonInput${contenedorNum}`;
+  let nombre = document.getElementById(inputId).value.trim().toLowerCase();
+  let urlApi = `https://pokeapi.co/api/v2/pokemon/${nombre}`;
+
+  // Manejo de errores
+  fetch(urlApi)
+    .then(respuesta => {
+      if (!respuesta.ok) {
+        throw new Error(`Error al obtener información del Pokémon: ${respuesta.statusText}`);
+      }
+      return respuesta.json();
+    })
+    .then(datosPokemon => mostrarPokemon(datosPokemon, contenedorNum))
+    .catch(error => mostrarError(contenedorNum, error)); // Llama a la función de manejo de errores
+};
+
+// Mostrar información (asumiendo que mostrarError está definida en otro lugar)
+const mostrarPokemon = (datosPokemon, contenedorNum) => {
+  let infoDivId = `pokemonInfo${contenedorNum}`;
+  let infoDiv = document.getElementById(infoDivId);
+
+  infoDiv.innerHTML = `
+    
+    <div class="card" style="width: 18rem;">
+  <img class = "PK-img" src="${datosPokemon.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title PK-name">${datosPokemon.name.toUpperCase()}</h5>
+    <p class="card-text">id: ${datosPokemon.id}</p>
+    <a href="#" class="btn btn-primary">añadir a mi equipo</a>
+  </div>
+</div>
+    `;
+};
+
+const mostrarError = (contenedorNum, error) => {
+    let infoDivId = `pokemonInfo${contenedorNum}`
+    let infoDiv = document.getElementById(infoDivId)
+    infoDiv.innerHTML= "No se encontró el Pokémon con ese nombre."
+}
