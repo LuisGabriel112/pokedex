e44c7bbce0cf28e69ce69cc259e80eda257e5c06
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2024-05-01 18:57:11 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+window.onload = function() {
+  // Código que quieres ejecutar después de que la página haya cargado completamente
+  let x = 0
+  while (x <= 11) {
+    x++
+    let aleatorioInt = Math.floor(Math.random()* 1008)
+  console.log("La página se ha cargado completamente");
+  let urlApi = `https://pokeapi.co/api/v2/pokemon/${aleatorioInt}`;
+
+  // Manejo de errores
+  fetch(urlApi)
+    .then(respuesta => {
+      if (!respuesta.ok) {
+        throw new Error(`Error al obtener información del Pokémon: ${respuesta.statusText}`);
+      }
+      return respuesta.json();
+    })
+    .then(datosPokemon => idAleatorio(datosPokemon))
+    .then(datosPokemon => separarTipo(datosPokemon) )
+};
+
+const idAleatorio = (datosPokemon) =>{
+    let infoDiv = document.getElementById('pokemonInfo1');
+
+  infoDiv.innerHTML += `
+    
+    <div class="card m-2" style="width: 18rem;">
+      <img class = "PK-img" src="${datosPokemon.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title PK-name">${datosPokemon.name.toUpperCase()}</h5>
+      <p class="card-text">id: ${datosPokemon.id}</p>
+      <p class="card-text">tipo: ${datosPokemon.types.map((type) => type.type.name)}</p>
+      <p></p>
+      <a href="#" class="btn btn-primary">añadir a mi equipo</a>
+    </div>
+  </div>
+    `;
+  
+    } 
+  }
+
+
 // Buscar Pokémon
 const buscarPokemon = (contenedorNum) => {
   let inputId = `pokemonInput${contenedorNum}`;
@@ -13,7 +55,6 @@ const buscarPokemon = (contenedorNum) => {
       return respuesta.json();
     })
     .then(datosPokemon => mostrarPokemon(datosPokemon, contenedorNum))
-    .then(datosPokemon => generar_poks(datosPokemon))
     .catch(error => mostrarError(contenedorNum, error)); // Llama a la función de manejo de errores
 };
 
@@ -37,22 +78,15 @@ const mostrarPokemon = (datosPokemon, contenedorNum) => {
     `;
 };
 
+const separarTipo = (datosPokemon) =>{
+  console.log('hola');
+} 
+
+
+addEventListener( "click", buscarPokemon );
 
 const mostrarError = (contenedorNum, error) => {
     let infoDivId = `pokemonInfo${contenedorNum}`
     let infoDiv = document.getElementById(infoDivId)
     infoDiv.innerHTML= "No se encontró el Pokémon con ese nombre."
 }
-
-const idAleatorio = () =>{
-  let aleatorioInt = Math.floor(Math.random()* 1008)
-
-  let i = 0
-  while (i <= 12) {
-    i++
-    aleatorioInt
-    console.log(aleatorioInt);
-  }
-} 
-
-idAleatorio()
